590a38505aaf7dc04210fdd1bf340086f1e1ab84
Z-sitawi <aaichaou.zakaria@gmail.com>
2025-02-14 00:06:00 +0100

    add salutation component and a dynamic navigation side bar

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,21 +1,32 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import "./styles/index.css";
-import App from "./App.jsx";
+
+import { StrictMode } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AuthPage from "./components/AuthPage.jsx";
-import Home from "./components/Home.jsx";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./config/store";
+
+/* Components */
+import User from "./components/User.jsx";
+import Admin from "./components/Admin.jsx";
+import App from "./App.jsx";
+import AuthPage from "./components/AuthPage.jsx";
+import Home from "./components/Home.jsx";
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route exact element={<App />} path="/" />
-          <Route element={<AuthPage />} path="/govroom/:action" />
-          <Route element={<Home />} path="/accueil" />
+          <Route exact path="/" element={<App />} />
+          <Route path="/govroom/:action" element={<AuthPage />} />
+          <Route path="/accueil" element={<Home />} />
+          <Route path="/accueil/user" element={<User />} />
+          <Route path="/accueil/admin" element={<Admin />} />
+
+          {/* Wrong Path */}
+          <Route path="*" element={<div className="display-1 text-center mt-5">Page Non Trouvée</div>} />
         </Routes>
       </BrowserRouter>
     </Provider>
